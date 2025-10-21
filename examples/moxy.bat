b283219e7edb12f2025519f12d5f1f4cc0689e0b
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-10-21 17:10:16 +0200

    feat: runnable namespace package

---
+++ examples/moxy.bat
@@ -0,0 +1,1 @@
+call hermes-cli -o .\ --config_file moxy.yml --experiment project=Test type=Moxy trial=0
---
+++ examples/moxy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hermes-cli -o ./ --config_file moxy.yml --experiment project=Test type=Moxy trial=0
